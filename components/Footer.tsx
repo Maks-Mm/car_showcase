47c0046555a281ef6712df8e36dd7933198fe8f7
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-04-22 22:10:47 +0200

    Car Catalogue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => {
       className="flex flex-col
   text-black-100 mt-5 border-t border-gray-100"
     >
-      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+      <div className="flex max-md:flex-col flex-wrap
+       justify-between gap-5 sm:px-16 px-6 py-10">
+        
         <div className="flex flex-col justify-start items-start gap-6">
           <Image
             src="/logo.svg"
@@ -42,6 +44,7 @@ const Footer = () => {
           ))}
         </div>
       </div>
+
       <div
         className="flex justify-between items-center flex-wrap 
           mt-10 border-t border-gray-100 sm:px-16 px-6 py-10"
