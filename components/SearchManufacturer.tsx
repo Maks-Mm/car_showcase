bfc151c9169cbac9555bfd14d6f2a4b16dc17d64
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-04-23 23:58:12 +0200

    car-logo.svg

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,18 +1,41 @@
 "use client";
-import { SearchManufacturerProps } from "@/types";
+
+import { useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { SearchManufacturerProps } from "@/types";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
+  const [query, setQuery] = useState("");
+
   return (
     <div className="search-manufacturer">
-      <Combobox >
-<div className="relative w-full">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car logo"
+            />
+          </Combobox.Button>
 
-</div>
-      
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+
+          <Transition>
+
+          </Transition>
+        </div>
       </Combobox>
     </div>
   );
