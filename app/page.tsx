e6570ab1bcb4ca0fa1856b6ade3321b0bb95c136
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-04-29 19:26:47 +0200

    Fuel + Year +
    es bestehen Fehler durch die ich keine Bilder unter den Suchfeld heruasziehen kann (even·tu·ell legt das Problem bei Modulen vor)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Image from "next/image";
-import { Hero, SearchBar, CustomFilter } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
+import {fuels,yearsOfProduction} from '@/constants';
 
 export default async function Home() {
   const allCars = await fetchCars();
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
-
-  console.log(isDataEmpty,"moin,halochan");
 
   return (
     <main className="overflow-hidden">
@@ -24,15 +23,25 @@ export default async function Home() {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={""} />
-            <CustomFilter title="year" options={"yearsOfProduction"} />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
-      </div>
 
-      <div className="home__error-container">
-        <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-        <p></p>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops,no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
