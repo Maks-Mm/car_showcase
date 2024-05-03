45c8c135f7485dd5d5750f3bc0deebcd8a83bb94
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-05-03 22:52:07 +0200

    use client von SearchManufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,39 +1,41 @@
 "use client";
+
+import Image from "next/image";
 import React, { useState } from "react";
 
-import SearchManufacturer from "@/components/SearchManufacturer";
-import { manufacturers } from "@/constants";
-import Image from "next/image";
+import SearchManufacturer from "./SearchManufacturer";
 
-const SearchButton = ({otherClasses}:{
-  otherClasses:string
-}) => (
-<button type="submit" 
-className={`-ml-3 z-10 ${otherClasses}`}
->
-  <Image
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   
-  />
-
-</button>
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+       <Image
+      src={"/magnifying-glass.svg"}
+      alt={"magnifying glass"}
+      width={40}
+      height={40}
+      className='object-contain'
+    />
+  </button>
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
+
+  const [manufacturer, setManufacturer] = useState('');
 
   const handleSearch = () => {};
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item">
-        <SearchManufacturer
-          manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
-        />
-        <SearchButton
-        otherClasses="sm:hidden"
-        />
-      </div>
+   <div className="searchbar__item">
+    <SearchManufacturer manufacturer={manufacturer} setManuFacturer={setManufacturer}
+    />
+<SearchButton
+otherClasses="sm:hidden"/>
+   </div>
+   <div className="">
+
+   </div>
     </form>
   );
 };
