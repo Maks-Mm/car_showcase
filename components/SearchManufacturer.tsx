fec118e757347393b28e11951b53360db57f019b
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-05-05 20:29:30 +0200

    GlassLupe und Alert

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -33,6 +33,7 @@ const SearchManufacturer = ({
               height={30}
               className="ml-4"
               alt="Car logo"
+              style={{padding:"top-10px"}}
             />
           </Combobox.Button>
 
