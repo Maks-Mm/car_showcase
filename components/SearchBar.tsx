7b04bd2a3fe9e82e6a295cd8eda8f3c41e947894
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-05-08 01:16:03 +0200

    keine Gute Anderungen

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -19,23 +19,25 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState("");
-  const [model, setModel] = useState("");
+  const [searchManufacturer, setSearchManufacturer] = useState("");
+  const [searchModel, setSearchModel] = useState("");
 const router = useRouter();
 
 
   const handleSearch = (e:React.FormEvent<HTMLFormElement>  ) => {
     e.preventDefault();
 
-    if(manufacturer === '' && model === '') {
+    if(searchManufacturer === '' && searchModel === '') {
       return alert('Please fill in the search bar')
     }
-    updateSearchParams(model.toLowerCase(),manufacturer.toLowerCase());
+  setModel(searchModel)
+  setSearchManufacturer(searchManufacturer)
   }
 
   const updateSearchParams = (model:string, manufacturer:string) => {
 const searchParams = new URLSearchParams(window.location.search);
 
+/*
 if(model){
   searchParams.set('model',model)
 }else{
@@ -51,6 +53,7 @@ const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 router.push(newPathname)
 
   }
+  */
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
