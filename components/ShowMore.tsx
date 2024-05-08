e327c828394cfafcbc9b4cb1f58ef04cc8e5855e
Maks Pryshchepa <Mmpryshchepa@gmail.com>
2024-05-09 00:49:17 +0200

    router.push

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,14 +3,17 @@
 import React from 'react'
 import { ShowMoreProps } from '@/types';
 import { useRouter } from 'next/navigation';
-import { CustomButton } from '.';
+import  CustomButton  from './CustomButton';
+import { updateSearchParams } from '@/utils';
 
-const ShowMore = ({pageNumber,isNext}:ShowMoreProps) => 
-  {
-
-    const router = useRouter();
+const ShowMore = ({pageNumber,isNext}:ShowMoreProps) =>  {
+const router = useRouter();
 
     const handleNavigation = () => {
+      const newLimit = (pageNumber + 1) * 10;
+const newPathName = updateSearchParams("limit",`${newLimit}`);
+
+router.push(newPathName);
 
     }
 
@@ -20,7 +23,7 @@ const ShowMore = ({pageNumber,isNext}:ShowMoreProps) =>
         <CustomButton
         title='Show More'
         btnType='button'
-        containerStyles='bg-primary- rounded-full text-white'
+        containerStyles='bg-primary-blue rounded-full text-white'
         handleClick={handleNavigation}
         />
       )}
